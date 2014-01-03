3596d09da168ec73a7d3f48332c808871a71e05d
Hari S R <mailtohsr@gmail.com>
2014-01-03 19:45:30 +0530

    Create patcher.sh

---
+++ patcher.sh
@@ -0,0 +1,9 @@
+ANDROID=${PWD}
+
+# Fix Compilation Issue #1
+cd ${ANDROID}/hardware/libhardware_legacy
+git fetch https://github.com/TamsuiCM11/android_hardware_libhardware_legacy refs/heads/stable/cm-11.0 && git cherry-pick 922d0c8ce846517fec01f129f2450d708c764220
+
+# Fix Compilation Issue #2
+cd ${ANDROID}/system/core
+git fetch https://github.com/TamsuiCM11/android_system_core refs/heads/stable/cm-11.0 && git cherry-pick 5b874ab6cbdf39ff69bbb3d55bace8091d71dc1^..50ad89e1d280b38278d4c84ad9e980d15dccb919
